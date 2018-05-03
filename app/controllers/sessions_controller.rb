ef1796a20e0bbbd92a925bc4d0652a84ec2ccbc2
EC2 Default User <ec2-user@ip-172-31-39-228.ap-southeast-1.compute.internal>
2018-05-03 10:41:54 +0000

    chap8 login failure finished

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
+  #GET /login
   def new
   end
+  
+  #POST /login
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user[:password_digest] && user.authenticate(params[:session][:password])
+      #password_digestがなかった場合もlogin拒否にする
+      
+      #true
+    else
+      flash.now[:danger] = "email or password error!"
+      render "new"
+    end
+  end
+  
 end
