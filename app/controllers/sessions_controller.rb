20f1f1347e2f8d608c5889cc745eb43a238cac2c
EC2 Default User <ec2-user@ip-172-31-39-228.ap-southeast-1.compute.internal>
2018-05-03 13:16:50 +0000

    chap8 middle

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,8 @@
 class SessionsController < ApplicationController
+  
+  #controlerで、Sessions_helperを使用できるようにする viewならばいらない
+  include SessionsHelper
+  
   #GET /login
   def new
   end
@@ -8,7 +12,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email])
     if user && user[:password_digest] && user.authenticate(params[:session][:password])
       #password_digestがなかった場合もlogin拒否にする
-      
+      log_in user
+      redirect_to
       #true
     else
       flash.now[:danger] = "email or password error!"
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,29 @@
 module SessionsHelper
+  
+  # 渡されたユーザーでログインする
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # 現在ログインしているユーザの情報を確認する
+  def current_user
+    #if @current_user == nil
+      #@current_user
+    #else
+      #@current_user = User.find_by(id: session[:user_id])
+    #end
+    
+    #うえと同じ
+    #@current_user = @current_user || User.find_by(id: session[:user_id])
+    
+    #うえと同じ
+    @current_user ||= User.find_by(id: session[:user_id])
+    
+  end
+  
+  # ユーザーがログインしていればtrue、その他ならfalseを返す
+  def logged_in?
+    !current_user.nil?
+  end
+  
 end
