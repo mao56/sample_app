e0bd3c5a79d0aba9dbeea9c903466bc84c27d242
EC2 Default User <ec2-user@ip-172-31-39-228.ap-southeast-1.compute.internal>
2018-05-07 14:24:18 +0000

    chap10 edit finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,28 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  #GET /users/:id/edit 
+  # => /app/views/users/edit.html.erb
+  def edit
+    @user = User.find(params[:id])
+  end
  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      #Success
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      #Failure
+      # => @user.error.full_messages()
+      render "edit"
+    end    
+  end
+  
+  private
+  
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
