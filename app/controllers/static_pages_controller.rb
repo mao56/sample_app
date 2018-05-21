c97151776ec287c7236962c9239360b5af49262f
EC2 Default User <ec2-user@ip-172-31-39-228.ap-southeast-1.compute.internal>
2018-05-21 11:58:28 +0000

    finished 13.3.1

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     # app/views/リソース名/アクション名.html.erb
+    @micropost = current_user.microposts.build if logged_in?
   end
 
   def help
