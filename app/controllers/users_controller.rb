b4187c57258fea09938d7b710480ba0cfac2127b
EC2 Default User <ec2-user@ip-172-31-39-228.ap-southeast-1.compute.internal>
2018-05-21 11:27:01 +0000

    chap13 middle

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   def show
     #@user = User.first
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
     #debugger
   end
   
@@ -64,15 +65,6 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
   
-   # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
-    
       # 正しいユーザーかどうか確認
     def correct_user
       @user = User.find(params[:id])
