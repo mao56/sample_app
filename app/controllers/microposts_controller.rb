24f315683d25eb9a579f59a6af82bc01b67d2137
EC2 Default User <ec2-user@ip-172-31-39-228.ap-southeast-1.compute.internal>
2018-05-22 11:55:17 +0000

    finished chap13

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -24,7 +24,7 @@ class MicropostsController < ApplicationController
   private
 
     def micropost_params
-      params.require(:micropost).permit(:content)
+      params.require(:micropost).permit(:content, :picture)
     end
     
     #DELETE /microposts/:id
