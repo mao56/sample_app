057f0394d2010470069355c8539d3eaaf5b2fa24
EC2 Default User <ec2-user@ip-172-31-39-228.ap-southeast-1.compute.internal>
2018-05-30 13:18:39 +0000

    finished list14.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
 
   has_many :microposts, dependent: :destroy
-  
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+                                  
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },format: { with: VALID_EMAIL_REGEX },uniqueness: { case_sensitive: false }
