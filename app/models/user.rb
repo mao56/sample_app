faeeacafa276a9262d447553eb49245387b70c1a
EC2 Default User <ec2-user@ip-172-31-39-228.ap-southeast-1.compute.internal>
2018-06-02 03:53:47 +0000

    before question

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,11 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
                                   dependent:   :destroy
-                                  
+                                 
+  #@user.active_relationships.map(&:followed)
+  has_many :following,through: 'active_relationships',
+                      source:  'followed'
+                      
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },format: { with: VALID_EMAIL_REGEX },uniqueness: { case_sensitive: false }
